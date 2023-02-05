4045f34a4aeed99c5a70c4f2db9b1c77d469dd4c
AndreiDaraban <72549078+andrei-db@users.noreply.github.com>
2023-02-05 03:57:26 +0200

    -created simple webpage with a navbar

---
+++ register.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Main page</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+    <header>
+        <nav id="navbar">
+            
+            <div id="nav-items">
+            <a href="index.php">Home</a>
+            <a href="search.php">Search</a>
+            </div>
+            <div id="login-register">
+                <a href="login.php">Login</a>
+                <a href="register.php">Register</a>
+            </div>
+        </nav>
+    </header>
+
+</body>
+
+</html>
